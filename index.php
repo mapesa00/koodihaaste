84ac695d0a0c7d4d0155ca1d7ffeb42f498a40b4
Petri Martikainen <petri.martikainen@edu.lapinamk.fi>
2021-06-03 13:36:59 +0300

    version 0.2
    Updated to use bootstrap 4.52

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     desc:   Koodihaasteen yksinkertainen ratkaisu käyttäen php:ta
 
 */
-session_start(); //käytetään session-tietoja käyttäjän tietojen tallentamiseen
+
 $kulutusKerroin=1.009;
 function sekunnitAjaksiKertoen($ss) {
   $s = $ss%60;
@@ -18,8 +18,9 @@ function sekunnitAjaksiKertoen($ss) {
   $palautus.=$m.' minuuttia ';
   $palautus.=$s.' sekuntia';
   return($palautus);
-  }
+}
 
+if(isset($_GET['auto1'])) $auto1=$_GET['auto1']; else $auto1='';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,41 +28,41 @@ function sekunnitAjaksiKertoen($ss) {
   <title>Matkalaskuri</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div class="itemcontainer">
-    <form action="valinta.php" method="post">
-      <ul>
+<div class="container">
+  <p></p> 
+	<h3 class="alert alert-info">Matkalaskuri</h3>
+    <form action="valinta.php" method="post">   
       <h4>Valitse auto, nopeus ja matka</h4>
       <input type="hidden" name="auto1" required>
       <label for="auto1">Valitse auto:</label>
-        <select class="oikeaanlaitaan" name="auto1">
+        <select class="form-group" name="auto1">
             <option value=''>--Valitse auto--</option>
-            <option value="3"<?php if($_GET['auto1']==3) echo ' selected'?>>Auto A: Kulutus 3l/100km/h</option>
-            <option value="3.5"<?php if($_GET['auto1']==3.5) echo ' selected'?>>Auto B: Kulutus 3.5l/100km/h</option>
-            <option value="4"<?php if($_GET['auto1']==4) echo ' selected'?>>Auto C: Kulutus 4l/100km/h</option>
+            <option value="3"<?php if($auto1==3) echo ' selected'?>>Auto A: Kulutus 3l/100km/h</option>
+            <option value="3.5"<?php if($auto1==3.5) echo ' selected'?>>Auto B: Kulutus 3.5l/100km/h</option>
+            <option value="4"<?php if($auto1==4) echo ' selected'?>>Auto C: Kulutus 4l/100km/h</option>
         </select>
-        <br>
         
-        <label for="matka">Anna matka kilometreinä:</label>
-        <input type="number" class="oikeaanlaitaan"  min="1" name="matka" value="<?php if(isset($_GET['matka'])) echo $_GET['matka'];else echo ''?>" required>
-        <br>
-        <label for="nopeus1">Nopeus yksi:</label>
-        <input type="number" class="oikeaanlaitaan" min="1" max="500" name="nopeus1" value="<?php if(isset($_GET['nopeus1'])) echo $_GET['nopeus1'];else echo ''?>" required>
-        <br>
-        <label for="nopeus2">Nopeus kaksi:</label>
-        <input type="number" class="oikeaanlaitaan" min="1" max="500" name="nopeus2" value="<?php if(isset($_GET['nopeus2'])) echo $_GET['nopeus2'];else echo ''?>" required>
-        <br><br>
-      <button type="submit" class="nappula">Suorita valinnat</button>
-      </ul>
+        <div class="form-group">
+          <label for="matka">Anna matka kilometreinä:</label>
+          <input type="number" class="form-control"  min="1" name="matka" value="<?php if(isset($_GET['matka'])) echo $_GET['matka'];else echo ''?>" required>
+        </div>
+        <div class="form-group">
+          <label for="nopeus1">Nopeus yksi:</label>
+          <input type="number" class="form-control" min="1" max="500" name="nopeus1" value="<?php if(isset($_GET['nopeus1'])) echo $_GET['nopeus1'];else echo ''?>" required>
+        </div>
+        <div class="form-group">
+          <label for="nopeus2">Nopeus kaksi:</label>
+          <input type="number" class="form-control" min="1" max="500" name="nopeus2" value="<?php if(isset($_GET['nopeus2'])) echo $_GET['nopeus2'];else echo ''?>" required>
+        </div>
+      <button type="submit" class="button">Suorita valinnat</button>      
     </form>
-  </div>
-<div class="itemcontainer3">
   <p>
     <?php
       if(isset($_GET['auto1'])) $auto1=$_GET['auto1'];
@@ -79,31 +80,42 @@ function sekunnitAjaksiKertoen($ss) {
           $aika2=($matka*1000)/($laskentaNopeus2/3.6);
 
           // lasketaan käytetty polttoainemäärä nopeuksille
-          $kulutus1=$auto1*pow($kulutusKerroin,$laskentaNopeus1);
-          $kulutus2=$auto1*pow($kulutusKerroin,$laskentaNopeus2);
+          $kulutus1=$auto1*pow($kulutusKerroin,($laskentaNopeus1-1));
+          $kulutus2=$auto1*pow($kulutusKerroin,($laskentaNopeus2-1));
           $litrat1=($matka*$kulutus1)/100;
           $litrat2=($matka*$kulutus2)/100;
           $aikaErotus=$aika2-$aika1;
-          $kulutusErotus=$litrat1-$litrat2;
-          
-          echo ('Nopeamman auton kulutus on '.round($litrat1,2).' litraa.<br> Aikaa matkaan meni '.sekunnitAjaksiKertoen($aika1).'.<br><br>');
+          $kulutusErotus=$litrat1-$litrat2;          
           if($aika1<>$aika2){
-            echo ('Taloudellisemman matkavauhdin kulutus on '.round($litrat2,2).' litraa.<br>');
-            echo ('Aikaa matkaan meni:'.sekunnitAjaksiKertoen($aika2).'.<br><br>');
+            echo ('<div class="alert alert-warning" role="alert">');
+            echo ('Nopeamman auton kulutus on '.round($kulutus1,3).' l/100km. Kokonaiskulutus matkalla oli '.round($litrat1,3).' litraa <br>');
+            echo ('Aikaa matkaan meni '.sekunnitAjaksiKertoen($aika1).'.');
+            echo ('</div>');
+            echo ('<div class="alert alert-success" role="alert">');
+            echo ('Taloudellisemman matkavauhdin kulutus on '.round($kulutus2,3).' l/100km/h. Kokonaiskulutus matkalla oli '.round($litrat2,3).' litraa<br>');
+            echo ('Aikaa matkaan meni '.sekunnitAjaksiKertoen($aika2).'.');
+            echo ('</div>');
+            echo ('<div class="alert alert-info" role="alert">');
             echo ('Valittu kovempi nopeus kulutti '.round($kulutusErotus,3).' litraa enemmän polttoainetta matkalla.<br>');
             echo (' Perillä oltiin '.sekunnitAjaksiKertoen($aikaErotus).' nopeammin.');
+            echo ('</div>');
           }else{
-            echo ('Valitsit saman nopeuden autoille!<br>Valinnoilla ei synny kulutus- tai aikaeroa matkalle!');
-
+            echo ('Autojen kulutus on '.round($litrat1,2).' litraa.<br> Aikaa matkaan meni '.sekunnitAjaksiKertoen($aika1).'.');
+            ?>
+            <div class="alert alert-primary" role="alert">
+              Valitsit saman nopeuden autoille!<br>Valinnoilla ei synny kulutus- tai aikaeroa matkalle!
+            </div>
+            <?php
           }
-          
-          
         }else{
-          echo "Valitse auto!";
+          ?>
+          <div class="alert alert-danger" role="alert">
+            Valitse auto!
+          </div>
+          <?php
       }
-
     ?>
   </p>  
-</div>
+  </div>
 </body>
 </html>
